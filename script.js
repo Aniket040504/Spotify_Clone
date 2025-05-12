41a44b6efb134fd73d85dd4e28ea4e3e89df9200
Aniket <aniket.bst454@gmail.com>
2025-05-13 01:10:28 +0530

    changes song and cover names

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let songIndex=0;
-let audioElement=new Audio("Asset/song.mp3");
+let audioElement=new Audio("Asset/Song/1.mp3");
 let masterPlay=document.getElementById('masterPlay');
 let progressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
+let isSeeking = false;
+
 
 
 let songs=[
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
-    {songName:"songname", filePath: 'Asset/song.mp3', coverPath: "Asset/cover.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/1.mp3', coverPath: "Asset/Cover/1.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/2.mp3', coverPath: "Asset/Cover/2.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/3.mp3', coverPath: "Asset/Cover/3.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/4.mp3', coverPath: "Asset/Cover/4.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/5.mp3', coverPath: "Asset/Cover/5.jpg"},
+    {songName:"songname", filePath: 'Asset/Song/6.mp3', coverPath: "Asset/Cover/6.jpg"},
 ]
 
 
@@ -34,13 +36,45 @@ masterPlay.addEventListener('click',()=>{
     }
 })
 
-audioElement.addEventListener('timeupdate', ()=>{
-    console.log("timeup");
-    //Seekbar
-    progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
-    console.log(progress);
-    progressBar.value=progress;
-})
+document.addEventListener('keydown', (e) => {
+
+    if (e.code === 'Space') {
+        e.preventDefault();
+
+        if (audioElement.paused) {
+            audioElement.play();
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+            gif.style.opacity=1;
+        } else {
+            audioElement.pause();
+            masterPlay.classList.remove('fa-pause-circle');
+            masterPlay.classList.add('fa-play-circle');
+            gif.style.opacity=0;    
+        }
+    }
+});
 
 
+//seekBar
 
+audioElement.addEventListener('timeupdate', () => {
+    if (!isSeeking) {
+        const progress = (audioElement.currentTime / audioElement.duration) * 100;
+        progressBar.value = progress;
+    }
+});
+
+progressBar.addEventListener('mousedown', () => {
+    isSeeking = true;
+});
+
+progressBar.addEventListener('mouseup', () => {
+    isSeeking = false;
+});
+
+progressBar.addEventListener('change', () => {
+    const seekTime = (progressBar.value * audioElement.duration) / 100;
+    audioElement.currentTime = seekTime;
+    isSeeking = false;
+});
